758d6b1dd3a201c22109eadc7f52b98b5c99db67
myplusher <my.plusher@gmail.com>
2023-04-25 16:53:14 +0300

    timeout

--- app/src/main/java/com/example/smartspace2/service/NetworkService.java
+++ app/src/main/java/com/example/smartspace2/service/NetworkService.java
@@ -1,5 +1,8 @@
 package com.example.smartspace2.service;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,7 +12,14 @@ public class NetworkService {
     private static final String BASE_URL = "http://10.0.2.2:8080";
 
     private NetworkService() {
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(40, TimeUnit.SECONDS)
+                .writeTimeout(40, TimeUnit.SECONDS)
+                .readTimeout(40, TimeUnit.SECONDS)
+                .build();
+
         mRetrofit = new Retrofit.Builder()
+                .client(okHttpClient)
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
